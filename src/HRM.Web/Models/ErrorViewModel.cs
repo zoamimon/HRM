efecbfb1ccdd8926ffb9216e13eb1c555449a733
zoamimon <zoamimon@hotmail.com>
2025-11-26 21:41:27 +0700

    Initial solution architecture setup

---
+++ src/HRM.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HRM.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
